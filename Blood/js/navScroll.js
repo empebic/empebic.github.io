ec6d54faa8360447c0cba00b0993891c11a026cc
empebic <57561646+empebic@users.noreply.github.com>
2022-01-28 23:11:01 +0100

    Null

--- Blood/js/navScroll.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $(document).scroll(function () {
-      var $nav = $(".navbar-fixed-top");
-      var $brand = $("#brand");
-      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-      $brand.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
-  });
